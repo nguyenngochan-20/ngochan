c0a58387ec726d56a8a6c2e4fb4f2c6ce4b10340
nguyenngochan-20 <ngochoang10092004@gmail.com>
2024-12-22 13:49:39 +0700

    Add files via upload

---
+++ TypeData/src/basic/dev/MainApp.java
@@ -0,0 +1,15 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nguyenngochan");
+		System.out.println("2450754");
+		System.out.println("lop 10A12");
+		System.out.println("Truong cao dang cong nghiep hue");
+		
+
+	}
+
+}
