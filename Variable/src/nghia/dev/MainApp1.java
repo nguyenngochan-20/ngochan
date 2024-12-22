a07d97896f8fbf50252c29530159477f5f1acfb3
nguyenngochan-20 <ngochoang10092004@gmail.com>
2024-12-22 14:18:11 +0700

    Add files via upload

---
+++ Variable/src/nghia/dev/MainApp1.java
@@ -0,0 +1,24 @@
+package nghia.dev;
+
+public class MainApp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = "Phan Van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		System.out.println("Ho va ten:");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+		final float PI = 3.14f;
+	}
+
+	
+
+
+	}
+
+
